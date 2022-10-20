7b5988cf0270246ebb2dc3397f0bce6febcd8c29
gbstorey <gbstorey@asu.edu>
2022-10-20 15:15:37 -0700

    add cart button component and styling

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,5 @@
 import classes from "./Header.module.css"
-import React from "react"
+import HeaderCartButton from "./HeaderCartButton"
 
 const mealImage = "https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg"
 
@@ -7,7 +7,7 @@ export default function Header(props) {
     return <>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes.mainImage}>
             <img src= {mealImage} alt="pictures of food"/>
