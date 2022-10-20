2a60ce294ef2de739ec800352702abb004d29584
gbstorey <gbstorey@asu.edu>
2022-10-20 15:52:05 -0700

    Added meals components and stylng

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,10 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+import React from "react";
+
+export default function Meals() {
+    return <>
+        <MealsSummary />
+        <AvailableMeals />
+    </>
+}
