665522c4db127ca6e1d4106185e88754e7e17036
gbstorey <gbstorey@asu.edu>
2022-10-21 16:55:50 -0700

    Added cart context, useReducer functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,33 @@
 import classes from "./HeaderCartButton.module.css"
 import CartIcon from "../Cart/CartIcon"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import CartContext from "../../store/cart-context"
 
 export default function HeaderCartButton(props) {
+    const [btnIsHighlighted, setButtonIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() =>{
+        if (cartCtx.items.length ===0) {
+            return;
+        }
+        setButtonIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [cartCtx.items])
+
     return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
